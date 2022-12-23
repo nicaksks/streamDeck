a5fb080215d0e16f9835eda18ea540f169f0d69f
nica <100009274+nicaksks@users.noreply.github.com>
2022-12-23 11:11:39 -0300

    Add files via upload

--- streamDeck.cpp
+++ streamDeck.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <windows.h>
+#include "streamDeck.h"
+
+void console() {
+  ShowWindow(GetConsoleWindow(), SW_HIDE);
+  disable();
+};
+
+void disable() {
+  RegisterHotKey(NULL, 0, 0, 0x61);
+  RegisterHotKey(NULL, 0, 0, 0x62);
+  RegisterHotKey(NULL, 0, 0, 0x63);
+  RegisterHotKey(NULL, 0, 0, 0x64);
+  RegisterHotKey(NULL, 0, 0, 0x65);
+  RegisterHotKey(NULL, 0, 0, 0x66);
+  RegisterHotKey(NULL, 0, 0, 0x67);
+  RegisterHotKey(NULL, 0, 0, 0x68);
+  RegisterHotKey(NULL, 0, 0, 0x69);
+};
+
+void keyboard() {
+  if(GetKeyState(0x61) & 0x8000) {
+    system("start notepad");
+  } else if(GetKeyState(0x62) & 0x8000) {
+    system("start notepad");
+  };
+};
 
 int main() {
 
-  ShowWindow(GetConsoleWindow(), SW_HIDE);
+  console();
 
   while(true) {
-    if(GetAsyncKeyState(VK_NUMPAD1)) {
-      system("start notepad");
-    } else if(GetAsyncKeyState(VK_NUMPAD2)) {
-      system("start notepad");
-    };
-    Sleep(60);
+    keyboard();
+    Sleep(120);
   };
+
   return 0;
 }
